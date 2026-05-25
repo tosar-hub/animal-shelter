19392b08681dc89b560beb0e76ea29988c25caf1
tosar <tosar@yandex.ru>
2026-05-25 20:20:40 +0300

    Revert "+ gender & color"
    This reverts commit 7dcb91ce77539df34f8a43bff6e02b11da7f6180.

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,15 @@
-module.exports = function(eleventyConfig) {
-  // Копируем папку css в результат (если понадобится свой CSS)
+module.exports = function (eleventyConfig) {
+  // Копируем папку css (если нужны свои стили)
   eleventyConfig.addPassthroughCopy("css");
-  
+
   return {
     dir: {
       input: ".",
       output: "_site",
       includes: "_includes"
-    },
-    // Если деплоите на GitHub Pages в поддиректорию /animal-shelter/,
-    // раскомментируйте строку ниже и замените на имя репозитория
-    // pathPrefix: "/animal-shelter/"
+    }
+    // Если вы деплоите на GitHub Pages в поддиректорию (например, /animal-shelter/),
+    // раскомментируйте строку ниже и укажите имя репозитория:
+    pathPrefix: "/animal-shelter/"
   };
 };
